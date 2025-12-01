10db952a14ba71b324da171d94f69c72b9d0e9da
alineitgirl <queenalina2005@gmail.com>
2025-12-01 12:55:54 +0300

    implement a hoc for managing window states and add the first window - terminal

---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,72 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import useWindowStore from '../store/window.js'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { Draggable } from 'gsap/Draggable'
+
+gsap.registerPlugin(Draggable)
+
+const WindowWrapper = (Component, windowKey) => {
+
+    const Wrapped = (props) => {
+        const { windows, focusWindow } = useWindowStore()
+        const { isOpen, zIndex } = windows[windowKey];
+        const ref = useRef(null)
+
+        useLayoutEffect(() => {
+            const el = ref.current
+            if (!el) return
+
+            el.style.display = isOpen ? "block" : "none"
+        }, [isOpen]);
+
+        useGSAP(() => {
+            const el = ref.current
+            if (!el || !isOpen) return
+
+            el.style.display = "block";
+
+            gsap.fromTo(el, {
+                scale: 0.8,
+                opacity: 0,
+                y: 40
+            }, {
+                scale: 1,
+                opacity: 1,
+                y: 0,
+                duration: 0.4,
+                ease: "power3.out"
+            })
+        },{ dependencies: [isOpen] })
+
+        useGSAP(() => {
+            const el = ref.current
+            if (!el) return
+
+            const [instance] = Draggable.create(el, {
+                onPress: () => {
+                    focusWindow(windowKey);
+                }
+            })
+
+            return () => instance.kill();
+        }, []);
+
+        return (
+            <section
+                id={windowKey}
+                ref={ref}
+                className="absolute"
+                style={{ zIndex }}
+            >
+                <Component {...props} />
+            </section>
+        )
+    }
+
+    Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || 'Component'})`
+
+    return Wrapped
+}
+
+export default WindowWrapper
